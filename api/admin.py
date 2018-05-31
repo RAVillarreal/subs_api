253dd0f0575703164f2df2f74e8e263026242080
RAVillarreal <rafaelvillarreal2000@gmail.com>
2018-05-31 07:53:32 -0400

    refactoring

--- api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Subtitle
-
-# Register your models here.
-class SubtitleAdmin(admin.ModelAdmin):
-    fields = ('name', 'link', 'downloads')
-
-admin.site.register(Subtitle, SubtitleAdmin)
--- api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Subtitle
-
-class SubtitleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Subtitle
-        fields = ("name", "link", "downloads", "date")
